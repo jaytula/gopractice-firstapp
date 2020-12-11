f0a554672ca450aea778b4facb0a00babb11fc2c
Jay T <jaytula@gmail.com>
2020-12-11 02:19:20 -0800

    In 1:03:11

--- lesson04/Main.go
+++ lesson04/Main.go
@@ -26,5 +26,26 @@ func main() {
 	var c bool
 	fmt.Printf("%v, %T\n", c, c)
 
+	// Zero value for all numeric types is '0' or the equivalent of '0' for that type
+	intvar := 42
+	fmt.Printf("%v, %T\n", intvar, intvar)
+
+	// signed integer types
+	// - int8 (-128, 127)
+	// - int16 (-32768, 32767)
+	// - int32 (-2147483648, 2147483647)
+	// - int64
+
+	// unsigned integers example
+	var uintvar uint16 = 42
+	fmt.Printf("%v, %T\n", uintvar, uintvar)
+
+	// unsigned integer types
+	// - uint8 (0, 255)
+	// - uint16 (0, 65535)
+	// - uint32 (0, 4294967295)
+	// - NO uint64 
+
+	// There's also a type `byte` which an alias for `int8`
 
 }
