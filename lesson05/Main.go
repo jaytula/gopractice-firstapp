b14b535dc9d08f7ae4c0dba7d09977f37e7fae1e
Jay T <jaytula@gmail.com>
2020-12-19 13:16:42 -0800

    Created Main.go for lesson05 on Constants. In 1:27:07

---
+++ lesson05/Main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Lesson 05: Constants")
+	fmt.Println("- Naming convention")
+	fmt.Println("- Typed constants")
+	fmt.Println("- Untyped constants")
+	fmt.Println("- Enumerated constants")
+	fmt.Println("- Enumeration expressions")
+}
