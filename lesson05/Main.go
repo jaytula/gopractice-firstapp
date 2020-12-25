a6c59b6b9fcd1b613c800703e70741e61328cf08
Jay T <jaytula@gmail.com>
2020-12-25 13:34:57 -0800

    Bit masking example. 1:47:53 in.

--- lesson05/Main.go
+++ lesson05/Main.go
@@ -34,6 +34,18 @@ const (
 	YB
 )
 
+const (
+	isAdmin = 1 << iota
+	isHeadquarters
+	canSeeFinancials
+
+	canSeeAfrica
+	canSeeAsia
+	canSeeEurope
+	canSeeNorthAmerica
+	canSeeSouthAmerica
+)
+
 func main() {
 	fmt.Println("Lesson 05: Constants")
 	fmt.Println("- Naming convention")
@@ -79,4 +91,9 @@ func main() {
 
 	fileSize := 4000000000.
 	fmt.Printf("%.2fGB\n", fileSize/GB)
+
+	var roles byte = isAdmin | canSeeFinancials | canSeeEurope
+	fmt.Printf("%b\n", roles)
+	fmt.Printf("Is Admin? %v\n", isAdmin & roles == isAdmin)  // bit masking
+	fmt.Printf("Is HQ? %v\n", isHeadquarters & roles == isHeadquarters)  // bit masking
 }
