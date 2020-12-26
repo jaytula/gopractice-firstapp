a5dda4864016689f6342d28d92364be539988e7d
Jay T <jaytula@gmail.com>
2020-12-26 13:52:06 -0800

    Array Creation. In 1:50:33

---
+++ lesson06/Main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Lesson 06 - Arrays and Slices")
+	/*
+		Arrays
+			- Creation
+			- Built-in functions
+			- Working with arrays
+		
+		Slices
+			- Creation
+			- Built-in functions
+			- Working with slices
+	*/
+
+	// Initialize an array
+	grades := [3]int{56, 47, 89}
+	fmt.Printf("Grades: %v\n", grades)
+}
