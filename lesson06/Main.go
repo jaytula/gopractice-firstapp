14679e5a9f2ce0255f89e704c426bebe8655a59d
Jay T <jaytula@gmail.com>
2020-12-27 11:15:27 -0800

    More on array assignment basics 1:54:20

--- lesson06/Main.go
+++ lesson06/Main.go
@@ -21,4 +21,18 @@ func main() {
 	// Initialize an array
 	grades := [3]int{56, 47, 89}
 	fmt.Printf("Grades: %v\n", grades)
+
+	// Initialize an array literal with size implicitly determined
+	grades2 := [...]int{56, 47, 89}
+	fmt.Printf("Grades: %v\n", grades2)
+
+	// Declare empty array and assign afterwards
+	var students [3]string;
+	fmt.Printf("Students: %v\n", students)
+	students[0] = "Lisa"
+	students[1] = "Jisoo"
+	students[2] = "Rose"
+	fmt.Printf("Students: %v\n", students)
+	fmt.Printf("Student #2: %v\n", students[1])
+
 }
