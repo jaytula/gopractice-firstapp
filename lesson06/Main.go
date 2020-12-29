539f545e55e16686eaf48909c96887acd17df739
Jay T <jaytula@gmail.com>
2020-12-29 00:43:05 -0800

    Arrays are values. 1:57:17 in

--- lesson06/Main.go
+++ lesson06/Main.go
@@ -36,6 +36,24 @@ func main() {
 	fmt.Printf("Student #2: %v\n", students[1])
 
 	// Determine length
-	fmt.Printf("Number of Studnts: %v\n", len(students))
+	fmt.Printf("Number of Students: %v\n", len(students))
+
+	// Arrays can be make up of any type but all elements have to be the same type
+	var identityMatrix [3][3]int = [3][3]int{ [3]int{1, 0, 0}, [3]int{0, 1, 0}, [3]int{0, 0, 1}}
+	fmt.Printf("Identity Matrix: %v\n", identityMatrix )
+
+	// Same thing
+	var identityMatrix2 [3][3]int
+	identityMatrix2[0] = [3]int{1, 0, 0}
+	identityMatrix2[1] = [3]int{0, 1, 0}
+	identityMatrix2[2] = [3]int{0, 0, 1}
+	fmt.Printf("Identity Matrix2: %v\n", identityMatrix2 )
+
+	// Arrays are considered values (not references)
+	a := [...]int{1,2,3}
+	b := a
+	b[1] = 5
+	fmt.Println(a) // [1, 2, 3]
+	fmt.Println(b) // [1, 5, 3]
 
 }
