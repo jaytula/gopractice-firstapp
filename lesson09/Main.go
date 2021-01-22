9048e63c83a7f763eb9b97fe493e62302d682e31
Jay T <jaytula@gmail.com>
2021-01-22 09:30:00 -0800

    For loop variations. In 3:32:22

--- lesson09/Main.go
+++ lesson09/Main.go
@@ -10,7 +10,7 @@ func main() {
 	//   - simple loops
 	//   - exiting early
 	//   - looping through collections
-	
+
 	// Most basic for-loop
 	for i := 0; i < 5; i++ {
 		fmt.Println(i)
@@ -29,5 +29,42 @@ func main() {
 		} else {
 			i = 2*i + 1
 		}
+	}
+
+	// Can omit first initializer in for be declaring it outside
+	j := 0
+	for ; j < 5; j++ {
+		fmt.Println(j)
+	}
+
+	// Can also eliminat incrementer
+	for j < 5 {
+		fmt.Println(j)
+		j++
+	}
+
+	// For loop dropping the semi-colons with only condition
+	k := 0
+	for k < 5 {
+		fmt.Println(j)
+		k++
+	}
+
+	// For-loop with nothing. Acts as a while loop. Use 'break' to get out
+	k2 := 0
+	for {
+		fmt.Println(k2)
+		k2++
+		if k2 == 5 {
+			break
+		}
+	}
+
+	// Using continue with for loops
+	for k3 := 0; k3 < 10; k3++ {
+		if k3%2 == 0 {
+			continue
+		}
+		fmt.Println(k3)
 	}
 }
