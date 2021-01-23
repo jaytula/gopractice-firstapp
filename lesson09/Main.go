002978343b8b8f773244cc9fbfd411fc22960523
Jay T <jaytula@gmail.com>
2021-01-23 09:45:23 -0800

    Finish on Looping. In 3:41:34

--- lesson09/Main.go
+++ lesson09/Main.go
@@ -67,4 +67,68 @@ func main() {
 		}
 		fmt.Println(k3)
 	}
+
+	// Nested loop
+	fmt.Println("Nested Loop")
+	var c int
+	var d int
+	for c = 1; c <= 3; c++ {
+		for d = 1; d <= 3; d++ {
+			fmt.Println(c * d)
+		}
+	}
+
+	fmt.Println("Nested Loop. Break out if result is greater or equal to 3")
+	out:
+	for c = 1; c <= 3; c++ {
+		for d = 1; d <= 3; d++ {
+			fmt.Println(c * d)
+			if c * d >= 3 {
+				break out
+			}
+		}
+	}
+
+	fmt.Println("Loop through items of slice using range")
+	s := []int{1, 2, 3}
+	for k, v := range s {
+		fmt.Println(k, v)
+	}
+
+	fmt.Println("Loop through items of array using range")
+	arr := [3]int{1, 2, 3}
+	for k, v := range arr {
+		fmt.Println(k, v)
+	}
+
+	fmt.Println("Loop through items of map using range")
+	map1 := map[string]int{
+		"California": 39250017,
+		"Texas": 27862596,
+		"Florida": 20612439,
+		"New York": 19745289,
+	}
+	for k, v := range map1{
+		fmt.Println(k, v)
+	}
+
+	fmt.Println("Loop through string using range")
+	s1 := "hello Go"
+	for k, v := range s1 {
+		fmt.Println(k, v, string(v))
+	}
+
+	// We can also loop through channels. Discussed later.
+
+	fmt.Println("If values are not needed")
+	for k := range s1 {
+		fmt.Println(k)
+	}
+
+	fmt.Println("If keys are not needed")
+	for _, v := range s1 {
+		fmt.Println(v)
+	}
+
+
 }
