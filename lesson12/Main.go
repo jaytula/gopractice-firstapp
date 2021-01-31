adb17cd6b1066865954e92c11b0bc916da896ce8
Jay T <jaytula@gmail.com>
2021-01-31 01:53:39 -0800

    Functions. Pointer parameters and variadic params. In 4:32:59

--- lesson12/Main.go
+++ lesson12/Main.go
@@ -18,6 +18,13 @@ func main() {
 	fmt.Println("Hello, functions")
 	sayMessage("Hello Go!")
 	sayMessageTwoParams("Hello Two Params", 27)
+	greeting := "Helloo"
+	name := "Stacey"
+	sayGreeting(greeting, name)
+	fmt.Println(name)
+	sayGreetingPointers(&greeting, &name)
+	fmt.Println(name)
+  sum("The sum is ", 1,2,3,4,5)
 }
 
 // Example of a function with one parameter
@@ -29,4 +36,24 @@ func sayMessage(msg string) {
 func sayMessageTwoParams(msg string, idx int) {
 	fmt.Println(msg)
 	fmt.Println("The value of the index is", idx)
+}
+
+func sayGreeting(greeting, name string) {
+	fmt.Println(greeting, name)
+	name = "Ted"
+}
+
+func sayGreetingPointers(greeting, name *string) {
+	fmt.Println(*greeting, *name)
+	*name = "Ted"
+}
+
+// Variadic parameter. Can only hav one. Has to be at the end. The variadic parameter becomes a slice.
+func sum(msg string, values ...int) {
+	fmt.Println(values)
+	result := 0
+	for _, v := range values {
+		result += v
+	}
+	fmt.Println(msg, result)
 }
