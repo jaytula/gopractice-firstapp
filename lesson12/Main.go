05cb1b77f4e818a0adb69784ab6b70adacb57383
Jay T <jaytula@gmail.com>
2021-02-01 15:05:14 -0800

    Function return values. In 4:42:16

--- lesson12/Main.go
+++ lesson12/Main.go
@@ -24,7 +24,23 @@ func main() {
 	fmt.Println(name)
 	sayGreetingPointers(&greeting, &name)
 	fmt.Println(name)
-  sum("The sum is ", 1,2,3,4,5)
+	sum("The sum is ", 1,2,3,4,5)
+	
+	s := sumWithReturn(1,2,3,4,5,6)
+	fmt.Println("sumWithReturn:", s)
+
+	sp := sumWithPointer(1,2,3,4,5,6,7)
+	fmt.Println("sumWithPointer:", sp, *sp)
+
+	sr := sumWithNamedReturn(9, 8, 7)
+	fmt.Println("sumWithNamedReturn", sr)
+
+	dr, err := divide(5.0, 0.0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(dr)
 }
 
 // Example of a function with one parameter
@@ -56,4 +72,37 @@ func sum(msg string, values ...int) {
 		result += v
 	}
 	fmt.Println(msg, result)
+}
+
+func sumWithReturn(values ...int) int {
+	result := 0
+	for _, v := range values {
+		result += v
+	}
+	return result
+}
+
+// Go allows returning pointers
+func sumWithPointer(values ...int) *int {
+	result := 0
+	for _, v := range values {
+		result += v
+	}
+	return &result
+}
+
+// Named return value
+func sumWithNamedReturn(values ...int) (result int) {
+	for _, v := range values {
+		result += v
+	}
+	return
+}
+
+// Multiple return values
+func divide(a, b float64) (float64, error) {
+	if b == 0.0 {
+		return 0.0, fmt.Errorf("Cannot divide by zero")
+	}
+	return a / b, nil
 }
