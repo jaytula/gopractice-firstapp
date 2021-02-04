b6c21beac4737d16469eb842add1f1901ead9808
Jay T <jaytula@gmail.com>
2021-02-04 13:59:07 -0800

    Add lesson13/Main.go for Interfaces topic with list of subtopics

---
+++ lesson13/Main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// - Basics
+// - Composing interfaces
+// - Type conversion
+//   - The empty interfaces
+//   - Type switches
+// - Implementing with values vs pointers
+// - Best practices
+
+func main() {
+	fmt.Println("Interfaces!!!")
+}
