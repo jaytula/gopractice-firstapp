a8273f6fc497f609a527cf421977cacf86685090
Jay T <jaytula@gmail.com>
2021-02-05 15:48:43 -0800

    Example method in *IntCounter (which is a type alias for int) In 5:08:27

--- lesson13/Main.go
+++ lesson13/Main.go
@@ -21,6 +21,12 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(n)
+
+	var myInt =  IntCounter(0)
+	var inc Incrementer = &myInt
+	for i := 0; i < 10; i++ {
+	  fmt.Println(inc.Increment())
+	}
 }
 
 // Writer Interfaces describe behavior. So instead of data, we have methods
@@ -34,4 +40,22 @@ type ConsoleWriter struct {}
 func (cw ConsoleWriter) Write(data []byte) (int, error) {
 	n, err := fmt.Println(string(data))
 	return n, err
+}
+
+// - Interface naming convention. If the interface has a single method `Read` then the interface name should be `Reader`
+// - For interface with multiple methods, you should name it by what it does.
+// - Most common way to use interface is to implement them on a struct but it doesn't have to be a struct
+
+// Incrementer increments stuff
+type Incrementer interface {
+	Increment() int
+}
+
+// IntCounter type alias for int
+type IntCounter int
+
+// Increment is method on *IntCounter type
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
 }
