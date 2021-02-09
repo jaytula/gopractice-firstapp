c785eb38fb0ce1e6288332145ca5f7c3b8fd6637
Jay T <jaytula@gmail.com>
2021-02-09 14:35:13 -0800

    Add Best Practices for intefaces. In 5:33:57

--- lesson13/Main.go
+++ lesson13/Main.go
@@ -185,3 +185,11 @@ func NewBufferedWriterCloser() *BufferedWriterCloser {
 		buffer: bytes.NewBuffer([]byte{}),
 	}
 }
+
+// Best Practices
+// - Use many, small interfaces
+//   - Single method interfaces are some o fthe most powerful and flexible
+//     - io.Writer, io.Reader, interface{}
+// - Don't export interfaces for types that will be consumed.  (export concrete type)
+// - Do export interfaces for types that will be used by package
+// - Design functions and methods to receive interfaces whenever possible
