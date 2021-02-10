b65c0f69c35a72ee82d12602ddd81c2cc22d4327
Jay T <jaytula@gmail.com>
2021-02-10 13:58:54 -0800

    Add lesson14 summary  GoRoutines. In 5:35:54

---
+++ lesson14/Main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Goroutines
+// - Creating goroutines
+// - Synchronization
+//   - WaitGroups
+//   - Mutexes
+// - Parallelism
+// - Best Practices
+
+func main() {
+  fmt.Println("GoRoutines")
+}
