26147cc7af89ef6fb798e49ef781a4ea7e02c678
Jay T <jaytula@gmail.com>
2021-02-11 15:40:43 -0800

    Three additional examples of go routines. In 5:44:05

--- lesson14/Main.go
+++ lesson14/Main.go
@@ -14,10 +14,46 @@ import (
 // - Best Practices
 
 func main() {
-  go sayHello() // Prefix with 'go' to spin off into a green thread. Light-weight abstraction for a thread
-	time.Sleep(100 * time.Millisecond)
+	// example01()
+	// example02()
+	example03()
+	example04()
 }
 
 func sayHello() {
 	fmt.Println("Hello")
+}
+
+func example01() {
+	go sayHello() // Prefix with 'go' to spin off into a green thread. Light-weight abstraction for a thread
+	time.Sleep(100 * time.Millisecond)
+}
+
+// example02 Immediately Invoked Function Expression
+func example02() {
+	var msg = "Hello IIFE"
+	go func() {
+		fmt.Println(msg)
+	}()
+	time.Sleep(100 * time.Millisecond)
+}
+
+// example03 This prints out 'Goodbye'
+func example03() {
+	var msg = "Hello IIFE"
+	go func() {
+		fmt.Println(msg)
+	}()
+	msg = "Goodbye"
+	time.Sleep(100 * time.Millisecond)
+}
+
+// example04 Fix
+func example04() {
+	var msg = "Hello example04"
+	go func(s string) {
+		fmt.Println(s)
+	}(msg)
+	msg = "Goodbye"
+	time.Sleep(100 * time.Millisecond)
 }
