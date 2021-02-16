166f78957831d0a1b035344fadccc3a0c3b307a2
Jay T <jaytula@gmail.com>
2021-02-16 00:47:18 -0800

    Add Goroutines summary. In 6:05:10

--- lesson14/Main.go
+++ lesson14/Main.go
@@ -22,6 +22,24 @@ import (
 //   - Check for race conditions at compie time
 //     - `go run --race main.go`
 
+// Summary
+// - Creating goroutines
+//   - Use `go` keyword in front of function call
+//   - When using anonymous functions, pass data as local variables
+// - Synchronization
+//   - Use sync.WaitGroup to wait for groups of goroutines to complete
+//   - Use sync.Mutex and sync.RWMutex to protect data access
+// - Parallelism
+//   - By default, Go will use CPU threads equal to avaiable cores
+//   - Change with runtime.GOMAXPROCS
+//   - More threads can increase performance, but too many can slow it down
+// - Best practices
+//   - Don't create goroutines in libraries
+//     - Let consume control concurrency
+//   - When creating a goroutine, know how it will end
+//     - Avoid suble memory leaks
+//   - Check for race conditions at compile time
+
 var wg = sync.WaitGroup{}
 
 func main() {
