4eb510695d312a4cf72bc7cc4f8a93a1a921f38e
Jay T <jaytula@gmail.com>
2021-02-17 16:41:06 -0800

    Start of Channels lesson. Add Agenda. In 6:07:41

---
+++ lesson15/Main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Agenda
+// - Channel basics
+// - Restricting data flow
+// - Buffered channels
+// - Closiing channels
+// - For...range loops with channels
+// - Select statements
+
+func main() {
+}
