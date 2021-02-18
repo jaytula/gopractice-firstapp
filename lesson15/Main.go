a480c7c7f8477795086a62fef1537052a034d18e
Jay T <jaytula@gmail.com>
2021-02-18 08:50:37 -0800

    Add basic exmaple of channel usage with waitgroup. In 6:11:46

--- lesson15/Main.go
+++ lesson15/Main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
 
 // Agenda
 // - Channel basics
@@ -12,5 +14,31 @@ import (
 // - For...range loops with channels
 // - Select statements
 
+var wg = sync.WaitGroup{}
+
 func main() {
+  example01()
+}
+
+// Create an int channel and send 42 into it in one goroutine
+// Other goroutine receives the int and prins it out.
+func example01() {
+	ch := make(chan int)
+	wg.Add(2)
+
+	// Receiving go routine
+	go func() {
+		i := <- ch
+		fmt.Println(i)
+		wg.Done()
+	}()
+
+	// Sending go routine
+	go func() {
+		i := 42
+		ch <- i  // passed by value
+		i = 27
+		wg.Done()
+	}()
+	wg.Wait()
 }
